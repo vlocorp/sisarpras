66758d9cd4359785f0e9d04b8dae1f1f84253468
ega wachid radiegtya <radiegtya@yahoo.co.id>
2012-08-16 14:52:52 +0700

    Revert "sisarpras v.0.1"
    This reverts commit 84608995d11eaaf3267fcdc160934ffd5cc85a54.

--- protected/extensions/vlo/flash/views/run.php
+++
@@ -1,6 +0,0 @@
-<!-- FLASH MESSAGE -->
-<?php foreach(Yii::app()->user->getFlashes() as $key => $message):?>
-<div class="flash-<?php echo $key;?>">
-    <?php echo $message;?>
-</div>
-<?php endforeach;?>
